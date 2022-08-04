78cbf01d817c92d37a0367d03e0dfdc39288584d
lovestar1325 <lovestar1325.lee@icloud.com>
2022-08-03 21:56:16 -0400

    add 'description'

--- SimpleTodoList/Model/TodoItem.swift
+++ SimpleTodoList/Model/TodoItem.swift
@@ -14,12 +14,14 @@ enum TodoStatus {
 struct TodoItem : Identifiable {
     let id: String
     let title: String
+    let description: String
     let deadline: Date
     let status: TodoStatus
     
-    init(id: String = UUID().uuidString, title: String, deadline: Date, status: TodoStatus) {
+    init(id: String = UUID().uuidString, title: String, description: String, deadline: Date, status: TodoStatus) {
         self.id = id
         self.title = title
+        self.description = description
         self.deadline = deadline
         self.status = status
     }
